0c7a037bb864e393876af9f82b7ae4dc99848128
elowanaud <elowan.audouin@icloud.com>
2025-07-01 19:46:31 +0200

    feat(backend): setup health check route

---
+++ apps/backend/start/health.ts
@@ -0,0 +1,10 @@
+import {
+	DiskSpaceCheck,
+	HealthChecks,
+	MemoryHeapCheck,
+} from "@adonisjs/core/health";
+
+export const healthChecks = new HealthChecks().register([
+	new DiskSpaceCheck(),
+	new MemoryHeapCheck(),
+]);
--- apps/backend/start/routes.ts
+++ apps/backend/start/routes.ts
@@ -1,14 +1,6 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from "@adonisjs/core/services/router";
 
+router.get("/health", "#controllers/health_checks_controller");
 router.get("/", async () => {
 	return {
 		hello: "world",
